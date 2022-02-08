a574bae8184fecb4a6d54df844594bd0cb0a5591
Nathan Boiron <nathan.boiron@gmail.com>
2022-02-08 01:36:48 +0100

    deploy to a sub-domain

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter basename={'/Loot-Tables'}>
+        <BrowserRoute>
             <App/>
-        </BrowserRouter>
+        </BrowserRoute>
     </React.StrictMode>,
     document.getElementById('root')
 );
